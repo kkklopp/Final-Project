4a0da27ad9836844c7fda9487afb206df7b8409c
Audrey <150072318+awoods24@users.noreply.github.com>
2023-11-18 13:45:41 -0500

    Add files via upload

---
+++ COSC 112 Final Project/README.txt
@@ -0,0 +1,17 @@
+# Final-Project
+
+INSTRUTIONS:
+
+javac Main.java // compile 
+java Main // run
+
+HOW TO SET THE ALARM:
+
+-Choose the time you want to set and add it in the following format (HH/mm), please be aware that you have to add a military formated time (24 hour format);
+-Click the "SET ALARM" button to create the new alarm;
+-Click the "SET ALARM" button to activate the alarm;
+-Wait for the time to come :);
+
+The code provided shows a functional alarm clock and a code that generates random math operations and check if it is correct. 
+At the moment, both systems are working separately, but the next steps are to integrate it to the math to come out when the alarm rings. 
+Also, we are working in a game so the user can choose between the game or the math to stop the alarm.\
---
+++ COSC 112 Final Project/src/AlarmClock/Alarm.java
@@ -0,0 +1,154 @@
+package AlarmClock;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import javax.sound.sampled.*;
+import MathAttempt.*;
+
+
+public class Alarm {
+    private int width;
+    private int height;
+    private int fps;
+    private JFrame frame;
+    private JPanel panel;
+    private JButton setButton;
+    private JButton onOffButton;
+    private JTextField hourField;
+    private JTextField minuteField;
+    private boolean alarmOn = false;
+    private LocalTime alarmTime;
+    public int AlertCount = 0;
+    boolean gameComplete = false;
+    static DisplayMathScreen mathProblem;
+
+    public Alarm(int initWidth, int initHeight, int initFPS) {
+        width = initWidth;
+        height = initHeight;
+        fps = initFPS;
+        frame = new JFrame("Alarm Clock");
+        panel = new JPanel();
+        setButton = new JButton("Set Alarm");
+        onOffButton = new JButton("Turn On");
+        hourField = new JTextField(2);
+        minuteField = new JTextField(2);
+
+        setButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setAlarm();
+            }
+        });
+
+        onOffButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                manageAlarm();
+            }
+        });
+
+        panel.add(new JLabel("Set Alarm (HH:mm):"));
+        panel.add(hourField);
+        panel.add(new JLabel(":"));
+        panel.add(minuteField);
+        panel.add(setButton);
+        panel.add(onOffButton);
+
+        frame.add(panel);
+        frame.setSize(300, 100);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
+        frame.setVisible(true);
+    }
+
+    private void setAlarm() {
+        try {
+            int hour = Integer.parseInt(hourField.getText());
+            int minute = Integer.parseInt(minuteField.getText());
+            if (hour >= 0 && hour <= 23 && minute >= 0 && minute <= 59) {
+                alarmTime = LocalTime.of(hour, minute);
+                System.out.println("Alarm set for " + alarmTime.format(DateTimeFormatter.ofPattern("HH:mm")));
+            } else {
+                JOptionPane.showMessageDialog(frame, "Invalid input. Please enter valid time values.");
+            }
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(frame, "Invalid input. Please enter numeric values.");
+        }
+    }
+
+    private void manageAlarm() {
+        if (alarmTime != null) {
+            alarmOn = !alarmOn;
+            //onOffButton.setText(alarmOn ? "Turn Off" : "Turn On");
+            if (alarmOn) {
+                startAlarm();
+            } else {
+                System.out.println("Alarm OFF");
+            }
+        } else {
+            JOptionPane.showMessageDialog(frame, "Please set the alarm first.");
+        }
+    }
+
+    private void startAlarm() {
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Clip clip = AudioSystem.getClip();
+                    AudioInputStream inputStream = AudioSystem.getAudioInputStream(
+                            getClass().getResourceAsStream("sound/alarm.wav"));
+                    clip.open(inputStream);
+                    while (alarmOn) {
+                        LocalTime currentTime = LocalTime.now();
+                        if (currentTime.getHour() == alarmTime.getHour() &&
+                                currentTime.getMinute() == alarmTime.getMinute()) {
+                            AlertCount = AlertCount + 1;
+                            if (AlertCount <= 1){
+                            SwingUtilities.invokeLater(new Runnable() {
+                                public void run() {
+                                    int option = JOptionPane.showOptionDialog(
+                                            frame,
+                                            "Wake Up!",
+                                            "Alarm",
+                                            JOptionPane.DEFAULT_OPTION,
+                                            JOptionPane.INFORMATION_MESSAGE,
+                                            null,
+                                            new Object[]{"Play to Stop!"},
+                                            "Play to Stop!");
+
+                                    stopAlarm(clip);
+                                }
+                                });
+    
+                            clip.loop(Clip.LOOP_CONTINUOUSLY);  };
+                        }
+
+/*
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+ */
+                    }
+    
+                    clip.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void stopAlarm(Clip sound) {
+        mathProblem = new DisplayMathScreen(width,height,fps);
+        if(mathProblem.mathInstance.complete) {
+            alarmOn = false;
+            onOffButton.setText("Turn On");
+            sound.stop();
+        }
+    }
+}
